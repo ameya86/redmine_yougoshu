61fea8d567ebb0506765c8a63a5c8b23c08265c5
OZAWA Yasuhiro <a_house_of_tomato@lion.ocn.ne.jp>
2014-01-22 20:20:52 +0900

    v0.0.1

--- lib/redmine_yougoshu/hook.rb
+++ lib/redmine_yougoshu/hook.rb
@@ -1,5 +1,5 @@
 module RedmineYougoshu
   class Hook < Redmine::Hook::ViewListener
-    render_on :view_layouts_base_body_bottom, :partial => 'yougoshu/layouts_base_body_bottom'
+    render_on :view_layouts_base_content, :partial => 'yougoshu/list'
   end
 end
--- lib/redmine_yougoshu/yougoshu.rb
+++ lib/redmine_yougoshu/yougoshu.rb
@@ -1,14 +1,51 @@
 module RedmineYougoshu
   class Yougoshu
+    # 用語集情報を格納したYAMLファイル
     YOUGOSHU_YAML = "#{Rails.root}/plugins/redmine_yougoshu/config/yougoshu.yml"
 
     @@yougo_map = nil
+    @@yougo_items = {}
 
+    # 用語集を取得
     def self.map
       return @@yougo_map if @@yougo_map
 
       @@yougo_map =YAML.load_file(YOUGOSHU_YAML).with_indifferent_access
       return @@yougo_map
+    end
+
+    # 用語集サイトのURL
+    def self.url
+      return self.map[:url]
+    end
+
+    # 現在のページに対応した用語集
+    def self.current_items(controller_name, action_name)
+      # キャッシュがあれば使う
+      return @@yougo_items[controller_name][action_name] if @@yougo_items[controller_name] && @@yougo_items[controller_name][action_name]
+
+      yougo_map = self.map
+
+      yougo = []
+      # 
+      yougo += yougo_map[:default] if yougo_map[:default]
+
+      # コントローラに対応した用語
+      controller_map = yougo_map[controller_name]
+      if controller_map
+        yougo += controller_map[:default] if controller_map[:default]
+        # アクションに対応した用語
+        yougo += controller_map[action_name] if controller_map[action_name]
+      end
+
+      # 重複を削除
+      yougo = yougo.sort.uniq
+
+      # キャッシュ
+      @@yougo_items[controller_name] = {}
+      @@yougo_items[controller_name][action_name] = yougo
+
+      return yougo
     end
   end
 end
